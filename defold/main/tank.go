baf6d74977ca57cab26faa38f518a1d83802c9ee
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-14 02:43:38 -0300

    fix: separate turret GO for independent rotation, fix shield tint, zoom in

--- defold/main/tank.go
+++ defold/main/tank.go
@@ -19,26 +19,6 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "turret"
-  type: "sprite"
-  data: "default_animation: \"white\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/gfx/game.atlas\"\n"
-  "}\n"
-  position {
-    x: 0.0
-    y: 4.0
-    z: 0.5
-  }
-  scale {
-    x: 4.0
-    y: 14.0
-    z: 1.0
-  }
-}
-embedded_components {
   id: "ring"
   type: "sprite"
   data: "default_animation: \"white\"\n"
@@ -70,7 +50,7 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.3
+    z: 0.6
   }
   scale {
     x: 28.0
---
+++ defold/main/turret.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"white\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/gfx/game.atlas\"\n"
+  "}\n"
+  position {
+    x: 0.0
+    y: 4.0
+    z: 0.0
+  }
+  scale {
+    x: 4.0
+    y: 14.0
+    z: 1.0
+  }
+}
